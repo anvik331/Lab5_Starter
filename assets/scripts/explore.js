9e2bd4e9105f2b80241dba4160e77d2bd3d7a7c3
En-Yu Hsu <a8hsu@ucsd.edu>
2026-05-04 21:36:50 -0700

    finish

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -9,12 +9,9 @@ function init() {
   const talkButton = document.querySelector('#explore button');
 
   let voices = [];
-
   function populateVoices() {
     voices = speechSynthesis.getVoices();
-
     voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
-
     voices.forEach(function(voice) {
       const option = document.createElement('option');
       option.value = voice.name;
@@ -22,32 +19,23 @@ function init() {
       voiceSelect.appendChild(option);
     });
   }
-
   populateVoices();
-
   speechSynthesis.addEventListener('voiceschanged', populateVoices);
-
   talkButton.addEventListener('click', function() {
     const text = textArea.value;
-
     const utterance = new SpeechSynthesisUtterance(text);
-
     const selectedVoice = voices.find(function(voice) {
       return voice.name === voiceSelect.value;
     });
-
     utterance.voice = selectedVoice;
-
     utterance.onstart = function() {
       faceImage.src = 'assets/images/smiling-open.png';
       faceImage.alt = 'Open mouth face';
     };
-
     utterance.onend = function() {
       faceImage.src = 'assets/images/smiling.png';
       faceImage.alt = 'Smiling face';
     };
-
     speechSynthesis.speak(utterance);
   });
 }
